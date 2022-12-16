f5947da200f0761966dcb69e8ba62343bbd5bfcc
LuigginaRossi <luigginavaldivia@gmail.com>
2022-12-16 15:37:15 +0100

    modifiche padding

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -14,32 +14,32 @@ export default {
         onSearchClick(){
             this.$emit("search", {...this.filters});  
         },
-        adultControlChange(){
-            this.store.includeAdult = false;
-            if(this.store.includeAdult === false){
-                alert("Alcuni contenuti sono adatti solo ai maggiorenni. confermi di avere più di 18 anni?");
-                return this.store.includeAdult = true;
-            }else{
-                return this.store.includeAdult = false;
-            }
-            fetchPosters()
-        },      
+        // adultControlChange(){
+        //     this.store.includeAdult = false;
+        //     if(this.store.includeAdult === false){
+        //         alert("Alcuni contenuti sono adatti solo ai maggiorenni. confermi di avere più di 18 anni?");
+        //         return this.store.includeAdult = true;
+        //     }else{
+        //         return this.store.includeAdult = false;
+        //     }
+        //     fetchPosters()
+        // },      
     },
 }
 </script>
 
 <template>
     <div class="d-flex justify-content-between align-content-center">
-        <h1 class="text-secondary">Boolflix</h1>
+        <h1 class="text-secondary py-3">Boolflix</h1>
 
 
-        <div class="input-group px-4">
+        <!-- <div class="input-group px-4">
                 <div class="input-group-text bg-none border-0">
                     <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input"
                     @change="adultControlChange">
                     <span class="px-2 text-secondary">Include adult movies</span> 
                 </div>
-            </div>
+        </div> -->
 
         <form @submit.prevent="onSearchClick" class="align-self-center d-flex">
 
